2c23e1e91b5806c6f4047954ff607cc5a9a16e73
laceyhunt <hunt3247@vandals.uidaho.edu>
2024-03-05 15:25:53 -0800

    Lacey Files

---
+++ latter/student/project.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Student Home</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel='stylesheet' href='../../nav.css'>
+</head>
+
+<body>
+    Project Info
+</body>
+
+</html>
