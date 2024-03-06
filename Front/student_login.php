35297c37b09cc815592370f3b00aa14a3443354d
laceyhunt <hunt3247@vandals.uidaho.edu>
2024-03-05 17:37:33 -0800

    demo ready

--- Front/student_login.php
+++ Front/student_login.php
@@ -25,7 +25,7 @@
         if ($rows == 1) {
             $_SESSION['username'] = $username;
             // Redirect to user dashboard page
-            header("Location: dashboard.php");
+            header("Location: ../latter/student/student_home.html");
         } else {
             echo "<div class='form'>
                   <h3>Incorrect Username/password.</h3><br/>
